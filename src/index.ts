cc8f8b2d0cf39e01bb56459f5deeb1bfe57cf8fb
william chang <wwwy3y3@gmail.com>
2018-07-23 16:23:51 +0800

    Release 1.1.0

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { RequiredActionAlias } from './defs/requiredActionProviderRepresentation';
+import { KeycloakAdminClient } from './client';
+
+export const requiredAction = RequiredActionAlias;
+export default KeycloakAdminClient;
