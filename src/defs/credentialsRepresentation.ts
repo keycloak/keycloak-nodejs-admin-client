ed9ebad49d3b88d7e186a487a8dfaf5b01103344
Alexis Solano <asolano@q6cyber.com>
2021-06-23 08:38:32 -0600

    PR Comments

--- src/defs/credentialsRepresentation.ts
+++ src/defs/credentialsRepresentation.ts
@@ -1,5 +1,3 @@
-[{"id":"c072522e-1a5a-4037-b8a9-aa8d94cda9f9","type":"otp","userLabel":"wfawf","createdDate":1623976183167,"credentialData":"{\"subType\":\"totp\",\"digits\":6,\"counter\":0,\"period\":30,\"algorithm\":\"HmacSHA1\"}"}]
-
 export type CredentialType = 'otp';
 
  export default interface CredentialRepresentation {
