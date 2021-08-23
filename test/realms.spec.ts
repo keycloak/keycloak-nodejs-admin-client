ea32f5e075bec2016725c30c003feb71cbbccbdb
Jon Koops <jonkoops@gmail.com>
2021-08-23 15:36:25 +0200

    Update test matrix with latest versions
    Changes:
    - Add Node 16 and remove Node 10 (all supported LTS versions)
    - Use latest version of Keycloak only
    - Use actions/setup-node@v2
    - Remove version specific test conditions
    - Skip localization test (not working on latest Keycloak)
    - Remove 'removeTotp' test (not supported by latest Keycloak)

--- test/realms.spec.ts
+++ test/realms.spec.ts
@@ -340,73 +340,69 @@ describe('Realms', () => {
     });
   });
 
-  if (
-    process.env.KEYCLOAK_VERSION &&
-    process.env.KEYCLOAK_VERSION.startsWith('12.')
-  ) {
-    describe('Realm localization', () => {
-      currentRealmName = 'master';
 
-      it('enable localization', async () => {
-        await kcAdminClient.realms.getRealmLocalizationTexts({realm: currentRealmName, selectedLocale: 'nl'});
-      });
+  describe('Realm localization', () => {
+    currentRealmName = 'master';
 
-      it('should add localization', async () => {
-        kcAdminClient.setConfig({
-          requestConfig: {headers: {'Content-Type': 'text/plain'}},
-        });
-        await kcAdminClient.realms.addLocalization(
-          {realm: currentRealmName, selectedLocale: 'nl', key: 'theKey'},
-          'value',
-        );
-      });
-
-      it('should get realm specific locales', async () => {
-        const locales = await kcAdminClient.realms.getRealmSpecificLocales({
-          realm: currentRealmName,
-        });
-
-        expect(locales).to.be.ok;
-        expect(locales).to.be.deep.eq(['nl']);
-      });
-
-      it('should get localization for specified locale', async () => {
-        const texts = await kcAdminClient.realms.getRealmLocalizationTexts({
-          realm: currentRealmName,
-          selectedLocale: 'nl',
-        });
-
-        expect(texts).to.be.ok;
-        expect(texts.theKey).to.be.eq('value');
-      });
-
-      it('should delete localization for specified locale key', async () => {
-        await kcAdminClient.realms.deleteRealmLocalizationTexts({
-          realm: currentRealmName,
-          selectedLocale: 'nl',
-          key: 'theKey',
-        });
-
-        const texts = await kcAdminClient.realms.getRealmLocalizationTexts({
-          realm: currentRealmName,
-          selectedLocale: 'nl',
-        });
-        expect(texts).to.be.ok;
-        expect(texts).to.be.deep.eq({});
-      });
-
-      it('should delete localization for specified locale', async () => {
-        await kcAdminClient.realms.deleteRealmLocalizationTexts({
-          realm: currentRealmName,
-          selectedLocale: 'nl',
-        });
-
-        const locales = await kcAdminClient.realms.getRealmSpecificLocales({
-          realm: currentRealmName,
-        });
-        expect(locales).to.be.ok;
-        expect(locales).to.be.deep.eq([]);
-      });
+    it.skip('enable localization', async () => {
+      await kcAdminClient.realms.getRealmLocalizationTexts({realm: currentRealmName, selectedLocale: 'nl'});
     });
-  }
+
+    it.skip('should add localization', async () => {
+      kcAdminClient.setConfig({
+        requestConfig: {headers: {'Content-Type': 'text/plain'}},
+      });
+      await kcAdminClient.realms.addLocalization(
+        {realm: currentRealmName, selectedLocale: 'nl', key: 'theKey'},
+        'value',
+      );
+    });
+
+    it.skip('should get realm specific locales', async () => {
+      const locales = await kcAdminClient.realms.getRealmSpecificLocales({
+        realm: currentRealmName,
+      });
+
+      expect(locales).to.be.ok;
+      expect(locales).to.be.deep.eq(['nl']);
+    });
+
+    it.skip('should get localization for specified locale', async () => {
+      const texts = await kcAdminClient.realms.getRealmLocalizationTexts({
+        realm: currentRealmName,
+        selectedLocale: 'nl',
+      });
+
+      expect(texts).to.be.ok;
+      expect(texts.theKey).to.be.eq('value');
+    });
+
+    it.skip('should delete localization for specified locale key', async () => {
+      await kcAdminClient.realms.deleteRealmLocalizationTexts({
+        realm: currentRealmName,
+        selectedLocale: 'nl',
+        key: 'theKey',
+      });
+
+      const texts = await kcAdminClient.realms.getRealmLocalizationTexts({
+        realm: currentRealmName,
+        selectedLocale: 'nl',
+      });
+      expect(texts).to.be.ok;
+      expect(texts).to.be.deep.eq({});
+    });
+
+    it.skip('should delete localization for specified locale', async () => {
+      await kcAdminClient.realms.deleteRealmLocalizationTexts({
+        realm: currentRealmName,
+        selectedLocale: 'nl',
+      });
+
+      const locales = await kcAdminClient.realms.getRealmSpecificLocales({
+        realm: currentRealmName,
+      });
+      expect(locales).to.be.ok;
+      expect(locales).to.be.deep.eq([]);
+    });
+  });
 });
