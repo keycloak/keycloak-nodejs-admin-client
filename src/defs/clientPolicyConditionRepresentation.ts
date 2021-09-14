4f5636e5e271d82d190a63bb12c280d8b35428d6
jenny-s51 <jshandel@redhat.com>
2021-09-14 09:11:38 -0400

    remove JsonNode and replace with object type

--- src/defs/clientPolicyConditionRepresentation.ts
+++ src/defs/clientPolicyConditionRepresentation.ts
@@ -1,9 +1,7 @@
-import JsonNode from './jsonNode';
-
 /**
  * https://www.keycloak.org/docs-api/15.0/rest-api/#_clientpolicyconditionrepresentation
  */
 export default interface ClientPolicyConditionRepresentation {
   condition?: string;
-  configuration?: JsonNode;
+  configuration?: object;
 }
