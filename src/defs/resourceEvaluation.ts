133d85315c41bdc9076c2903b7a320a7996c0f1f
Jenny <32821331+jenny-s51@users.noreply.github.com>
2022-01-07 06:11:40 -0500

    fix ResourceEvaluation (#360)

--- src/defs/resourceEvaluation.ts
+++ src/defs/resourceEvaluation.ts
@@ -1,7 +1,8 @@
 import ResourceRepresentation from './resourceRepresentation';
 
 export default interface ResourceEvaluation {
-  rolesIds?: string[];
+  roleIds?: string[];
+  clientId: string;
   userId: string;
   resources?: ResourceRepresentation[];
   entitlements: boolean;
