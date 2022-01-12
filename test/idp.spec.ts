f047ef32d2a196b32720ee4deb14d5dc9faaec54
Jon Koops <jonkoops@gmail.com>
2022-01-12 11:26:36 +0100

    Replace FakerJS with forked alternative

--- test/idp.spec.ts
+++ test/idp.spec.ts
@@ -2,7 +2,7 @@
 import * as chai from 'chai';
 import {KeycloakAdminClient} from '../src/client';
 import {credentials} from './constants';
-import faker from 'faker';
+import faker from '@faker-js/faker';
 const expect = chai.expect;
 
 describe('Identity providers', () => {
