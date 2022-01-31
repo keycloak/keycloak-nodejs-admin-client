cb8aeadeca4033a0a40703cf3e583b58e0449c5a
jenny-s51 <jshandel@redhat.com>
2022-01-31 10:08:53 -0500

    mark fields as optional

--- src/defs/policyResultRepresentation.ts
+++ src/defs/policyResultRepresentation.ts
@@ -2,8 +2,8 @@ import PolicyRepresentation, {DecisionEffect} from './policyRepresentation';
 import ScopeRepresentation from './scopeRepresentation';
 
 export default interface PolicyResultRepresentation {
-  policy: PolicyRepresentation;
-  status: DecisionEffect;
-  associatedPolicies: PolicyResultRepresentation[];
-  scopes: ScopeRepresentation[];
+  policy?: PolicyRepresentation;
+  status?: DecisionEffect;
+  associatedPolicies?: PolicyResultRepresentation[];
+  scopes?: ScopeRepresentation[];
 }
