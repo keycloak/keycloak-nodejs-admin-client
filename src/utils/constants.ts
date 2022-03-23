6c1487990a6704aeeb14cb75bbed5e0b79424cd0
Jon Koops <jonkoops@gmail.com>
2022-03-23 10:47:10 +0100

    Restore auth

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const defaultBaseUrl = 'http://127.0.0.1:8080';
+export const defaultBaseUrl = 'http://127.0.0.1:8080/auth';
 
 export const defaultRealm = 'master';
--- test/idp.spec.ts
+++ test/idp.spec.ts
@@ -155,10 +155,10 @@ describe('Identity providers', () => {
   it('Import from url', async () => {
     const result = await kcAdminClient.identityProviders.importFromUrl({
       providerId: 'oidc',
-      fromUrl: 'http://localhost:8080/realms/master/.well-known/openid-configuration',
+      fromUrl: 'http://localhost:8080/auth/realms/master/.well-known/openid-configuration',
     });
 
     expect(result).to.be.ok;
-    expect(result.authorizationUrl).to.equal('http://localhost:8080/realms/master/protocol/openid-connect/auth');
+    expect(result.authorizationUrl).to.equal('http://localhost:8080/auth/realms/master/protocol/openid-connect/auth');
   });
 });
