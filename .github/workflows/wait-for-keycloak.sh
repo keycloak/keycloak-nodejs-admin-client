e0f962a19dc3679e00c91990e77bb39f39725430
Jon Koops <jonkoops@gmail.com>
2022-03-23 10:51:23 +0100

    restore wait for

--- .github/workflows/wait-for-keycloak.sh
+++ .github/workflows/wait-for-keycloak.sh
@@ -5,7 +5,7 @@ PORT=${2:-8080}
 RETRIES=60
 
 echo -n "Waiting for keycloak to start on ${HOST}:${PORT}"
-until curl -f -s "http://${HOST}:${PORT}/auth/realms/master" > /dev/null
+until curl -f -s "http://${HOST}:${PORT}/realms/master" > /dev/null
 do
     RETRIES=$(($RETRIES - 1))
     if [ $RETRIES -eq 0 ]
