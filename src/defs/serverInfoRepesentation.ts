352892a71588369ae5b64923d10b56234ce8839e
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-10-06 00:09:51 +0200

    fixed type of locale on theme (#554)

--- src/defs/serverInfoRepesentation.ts
+++ src/defs/serverInfoRepesentation.ts
@@ -27,7 +27,7 @@ export interface ServerInfoRepresentation {
 
 export interface ThemeInfoRepresentation {
   name: string;
-  locales: string[];
+  locales?: string[];
 }
 
 export interface SpiInfoRepresentation {
