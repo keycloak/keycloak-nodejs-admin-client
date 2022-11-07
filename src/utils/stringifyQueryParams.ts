ceb8a563f461f0264afd7f157ab658b0ed84a713
Jon Koops <jonkoops@gmail.com>
2022-11-07 11:06:23 -0500

    Filter out empty array values for query params

--- src/utils/stringifyQueryParams.ts
+++ src/utils/stringifyQueryParams.ts
@@ -1,6 +1,14 @@
-export function stringifyQueryParams(params: Record<string, string | undefined>) {
+export function stringifyQueryParams(params: Record<string, unknown>) {
   return new URLSearchParams(
     Object.entries(params)
-      .filter((param): param is [string, string] => !!param[1])
+      .filter((param): param is [string, string] => {
+        const [, value] = param;
+
+        if (Array.isArray(value) && value.length === 0) {
+          return false;
+        }
+
+        return !!value;
+      })
   ).toString();
 }
