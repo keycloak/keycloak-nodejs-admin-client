5bb3b89ef64b020dbd88533624b29fa62455be97
Jon Koops <jonkoops@gmail.com>
2022-11-07 10:24:02 -0500

    Filter out empty string values in search params (#576)

--- src/utils/stringifyQueryParams.ts
+++ src/utils/stringifyQueryParams.ts
@@ -1,6 +1,6 @@
 export function stringifyQueryParams(params: Record<string, string | undefined>) {
   return new URLSearchParams(
     Object.entries(params)
-      .filter((param): param is [string, string] => param[1] !== undefined)
+      .filter((param): param is [string, string] => !!param[1])
   ).toString();
 }
